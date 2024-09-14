0622eecf63f875cfc1eecf48fb706b985095996a
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-09-14 13:39:33 -0300

    add: logger INFO and ERROR messages

--- rpa.py
+++ rpa.py
@@ -23,17 +23,29 @@ class NewsWebScraper:
 
     def set_webdriver(self, browser="Chrome"):
         options = self.set_chrome_options()
-        self.driver = start(browser, options=options)
+        try:
+            self.driver = start(browser, options=options)
+            self.logger.info("WebDriver started successfully.")
+        except Exception as e:
+            self.logger.error(f"ERR0R set_webdriver(): Failed to start WebDriver: {e}")
 
     def open_url(self, url: str):
         if self.driver:
-            self.driver.get(url)
+            try:
+                self.driver.get(url)
+                self.logger.info(f"Opened URL: {url}")
+            except Exception as e:
+                self.logger.error(f"ERROR open_url(): Failed to open URL {url}: {e}")
         else:
             self.logger.error("ERR0R open_url(): WebDriver not initialized. You must call set_webdriver() first.")
 
     def close_all(self):
         if self.driver:
-            self.driver.quit()
+            try:
+                self.driver.quit()
+                self.logger.info("WebDriver closed successfully.")
+            except Exception as e:
+                self.logger.error(f"ERROR close_all(): Failed to close WebDriver: {e}")
         else:
             self.logger.error("ERROR close_all(): WebDriver not initialized.")
 
@@ -41,13 +53,13 @@ class NewsWebScraper:
 def main():
     url = 'https://www.latimes.com/'
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     news_scraper = NewsWebScraper()
     news_scraper.set_webdriver()
     news_scraper.open_url(url)
 
-    time.sleep(4)
+    time.sleep(1)
 
     news_scraper.close_all()
 
