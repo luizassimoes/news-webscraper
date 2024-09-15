d637900c406a89d9052e46caae4721a1d9216796
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-09-15 00:05:13 -0300

    fix: replacing " for '

--- rpa.py
+++ rpa.py
@@ -6,8 +6,7 @@ import requests
 from openpyxl import Workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from RPA.core.webdriver import start
 
@@ -22,42 +21,42 @@ class NewsWebScraper:
         options = webdriver.ChromeOptions()
         # options.add_argument('--headless')
         options.add_argument('--no-sandbox')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-gpu')
         options.add_argument('--disable-web-security')
-        options.add_argument("--start-maximized")
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument('--start-maximized')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
-    def set_webdriver(self, browser="Chrome"):
+    def set_webdriver(self, browser='Chrome'):
         options = self.set_chrome_options()
         try:
             self.driver = start(browser, options=options)
-            self.logger.info("WebDriver started successfully.")
+            self.logger.info('WebDriver started successfully.')
         except Exception as e:
-            self.logger.error(f"ERR0R set_webdriver() | Failed to start WebDriver: {e}")
+            self.logger.error(f'ERR0R set_webdriver() | Failed to start WebDriver: {e}')
 
     def open_url(self, url: str):
         if self.driver:
             try:
                 self.driver.get(url)
-                self.logger.info(f"Opened URL: {url}")
+                self.logger.info(f'Opened URL: {url}')
             except Exception as e:
-                self.logger.error(f"ERROR open_url() | Failed to open URL {url}: {e}")
+                self.logger.error(f'ERROR open_url() | Failed to open URL {url}: {e}')
         else:
-            self.logger.error("ERR0R open_url() | WebDriver not initialized. You must call set_webdriver() first.")
+            self.logger.error('ERR0R open_url() | WebDriver not initialized. You must call set_webdriver() first.')
 
     def search(self, search_query: str):
         try:
             search_button = self.driver.find_element(By.XPATH, '/html/body/ps-header/header/div[2]/button')
             search_button.click()
-            self.logger.info(f"Search button clicked.")
+            self.logger.info(f'Search button clicked.')
             search_field = self.driver.find_element(By.XPATH, '/html/body/ps-header/header/div[2]/div[2]/form/label/input')
             search_field.send_keys(search_query)
             search_field.submit()
-            self.logger.info(f"Search query submitted.")
+            self.logger.info(f'Search query submitted.')
         except Exception as e:
-            self.logger.error(f"ERROR search() | Could not find element: {e}")
+            self.logger.error(f'ERROR search() | Could not find element: {e}')
 
     def sort_by_newest(self):
         wait = WebDriverWait(self.driver, 100)
@@ -67,9 +66,9 @@ class NewsWebScraper:
                 sort_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.select-input')))
                 select = Select(sort_element)
                 select.select_by_visible_text('Newest')
-            self.logger.info(f"Query sorted by Newest.")
+            self.logger.info(f'Query sorted by Newest.')
         except Exception as e:
-            self.logger.error(f"ERROR sort_by_newest() | Error during selecting option: {e}")
+            self.logger.error(f'ERROR sort_by_newest() | Error during selecting option: {e}')
         
     def get_element_list(self, element_selector, src=False):
         wait = WebDriverWait(self.driver, 100)
@@ -78,15 +77,15 @@ class NewsWebScraper:
         for attempt in range(retries):
             try: 
                 elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, element_selector)))
-                self.logger.info(f"{get_element} gotten on attempt {attempt+1}.")
+                self.logger.info(f'{get_element}s gotten on attempt {attempt+1}.')
                 if not src:
                     return [element.text for element in elements]
                 else:
                     return [element.get_attribute('src') for element in elements]
             except Exception as e:
-                self.logger.warning(f"Could not get {get_element} on attempt {attempt+1}. Retrying...")
+                self.logger.warning(f'Could not get {get_element} on attempt {attempt+1}. Retrying...')
                 if attempt == retries - 1:
-                    self.logger.error(f"ERROR get_element_list(): {get_element} | Error: {e}")
+                    self.logger.error(f'ERROR get_element_list(): {get_element} | Error: {e}')
 
     def get_news_titles(self):
         return self.get_element_list('h3.promo-title a')
@@ -106,7 +105,7 @@ class NewsWebScraper:
             title_count = titles[i].count(query)
             description_count = descriptions[i].count(query)
             query_count.append(title_count + description_count)
-        self.logger.info(f"Search query in Title counted.")
+        self.logger.info(f'Search query in Title counted.')
         return query_count
     
     def title_contains_money(self, titles):
@@ -122,7 +121,7 @@ class NewsWebScraper:
                     money = True
                     break
             money_in_query.append(money)
-        self.logger.info(f"Contains money analyzed.")
+        self.logger.info(f'Contains money analyzed.')
         return money_in_query
             
 
@@ -138,11 +137,11 @@ class NewsWebScraper:
         if self.driver:
             try:
                 self.driver.quit()
-                self.logger.info("WebDriver closed successfully.")
+                self.logger.info('WebDriver closed successfully.')
             except Exception as e:
-                self.logger.error(f"ERROR close_all() | Failed to close WebDriver: {e}")
+                self.logger.error(f'ERROR close_all() | Failed to close WebDriver: {e}')
         else:
-            self.logger.error("ERROR close_all() | WebDriver not initialized.")
+            self.logger.error('ERROR close_all() | WebDriver not initialized.')
 
     def to_excel(self, data):
         wb = Workbook()
@@ -156,9 +155,9 @@ class NewsWebScraper:
                 for i_row, val in enumerate(data[i_col]):
                     sheet.cell(row=i_row+2, column=i_col+1).value = val
             else:
-                self.logger.error(f"ERROR to_excel() | {header} list is None.")
+                self.logger.error(f'ERROR to_excel() | {header} list is None.')
         sheet.title = 'NEWS'
-        self.logger.info("Excel done.")
+        self.logger.info('Excel done.')
         return wb
 
 def main():
