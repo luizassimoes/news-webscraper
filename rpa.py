b1769e58e2fda431d9bbdae626a04b26e799a6b8
Luiza Souza Simões <luizassimoes@hotmail.com>
2024-09-16 16:04:15 -0300

    config: running on headless mode

--- rpa.py
+++ rpa.py
@@ -20,12 +20,14 @@ class NewsWebScraper:
 
     def set_chrome_options(self):
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-web-security')
         options.add_argument('--start-maximized')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--log-level=3")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
