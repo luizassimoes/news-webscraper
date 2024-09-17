2f8da797ee1cf40f841b30f99933b55fd186ceec
Luiza Souza Simões <luizassimoes@hotmail.com>
2024-09-17 15:50:23 -0300

    try: log level

--- tasks.py
+++ tasks.py
@@ -32,7 +32,7 @@ class NewsWebScraper:
         options.add_argument('--disable-web-security')
         options.add_argument('--start-maximized')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--log-level=3")
+        options.add_argument("--log-level=0")
         # options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
